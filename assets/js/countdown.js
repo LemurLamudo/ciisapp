0245f96be26f03748ad6d5ea93efaea8a49674b4
NainAceroMamani <a_nacerom@unjbg.edu.pe>
2021-06-09 21:57:18 -0500

    make it better

---
+++ assets/js/countdown.js
@@ -0,0 +1,28 @@
+// Setup End Date for Countdown (getTime == Time in Milleseconds)
+let launchDate = new Date("Nov 08, 2021 9:00:00").getTime();
+
+// Setup Timer to tick every 1 second
+let timer = setInterval(() => {
+  let now = new Date().getTime();
+  // Get the difference in time to get time left until reaches 0
+  let t = launchDate - now;
+  if(t > 0) {
+    // Algorithm to calculate days...
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    // prefix any number below 10 with a "0" E.g. 1 = 01
+    if(days < 10) { days = "0" + days; }
+    // Algorithm to calculate hours
+    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    if(hours < 10) { hours = "0" + hours; }
+    // Algorithm to calculate minutes
+    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    if(mins < 10) { mins = "0" + mins; }
+    // Algorithm to calc seconds
+    let secs = Math.floor((t % (1000 * 60)) / 1000);
+    if(secs < 10) { secs = "0" + secs; }
+    document.querySelector('.day').innerText = days;
+    document.querySelector('.hour').innerText = hours;
+    document.querySelector('.minute').innerText = mins;
+    document.querySelector('.second').innerText = secs;
+  }
+}, 1000);
---
+++ templates/includes/inc_header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Congreso Internacional de Informática y Sistemas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="_aCDFqLI3QdYZr7Nox7_RfyqGLpRlmLMEAOfEZQk3mQ">
+    <meta name="keywords" content="XXII CIIS, xxii ciis, CIIS XXII, ciis xxii, ciis, postmaster, feria tecnológica, tacna ciis, congreso internacional, conference, university, universidad">
+    <meta name="description" content="El Congreso Internacional de Informática y Sistemas (CIIS) brinda toda una gama de conocimiento con respecto a distintos temas de la informatica con ponentes del más alto nivel">
+    <meta property="og:title" content="XXII Congreso Internacional de Informática y Sistemas">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="El Congreso Internacional de Informática y Sistemas (CIIS)brinda todo una gama de conocimiento con respecto a distintos temas de la informatica con ponentes del más alto nivel">
+    <meta property="og:image" content="none">
+    <meta property="og:url" content="https://www.ciistacna.com/">
+    <meta property="og:site_name" content="XXII CIIS">
+    <link rel="manifest" href="<?php echo FONTS.'site.webmanifest' ?>">
+    <link rel="icon" href="<?php echo FONTS.'favicon-16x16.png' ?>" type="image/png" sizes="16x16">
+    <!-- <link rel="icon" href="assets/fav/favicon-32x32.png" type="image/png" sizes="32x32"> -->
+    <link rel="icon" href="<?php echo FONTS.'favicon-96x96.png' ?>" type="image/png" sizes="96x96">
+    <link rel="icon" href="<?php echo FONTS.'favicon-192x192.png' ?>" type="image/png" sizes="192x192">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo FONTS.'apple-touch-icon.png' ?>">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.6.2/plyr.css" />
+    <link rel="stylesheet" href="<?php echo CSS.'main.css' ?>">
+    
+</head>
+
+<body >
+<!-- ends inc_header.php -->
