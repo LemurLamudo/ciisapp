355101be2667e7680dddd4194d051bc92a939165
NainAceroMamani <a_nacerom@unjbg.edu.pe>
2021-08-10 23:22:40 -0500

    awdawd

--- app/models/postmasterModel.php
+++ app/models/postmasterModel.php
@@ -17,14 +17,14 @@
          * @return @List<Postmaster>
          */
         public function all(){
-            
+
             $sql = 'SELECT c.id, c.photo, c.name, p.anio, ps.name as name_pais , pc.nombre as name_tema, pc.status as status_ponencia, pc.id as ponencia_id, pc.asistencia, pc.hora_ini, pc.hora_fin
                 FROM users as c 
                 INNER JOIN ponente as p ON c.id = p.user_id 
                 INNER JOIN paises as ps ON ps.id = p.pais_id 
                 INNER JOIN ponencia as pc ON p.id = pc.ponente_id 
                 WHERE c.role = "ROLE_PONENTE"
-                ORDER BY ponencia.hora_ini';
+                ORDER BY pc.hora_ini ASC';
 
             try{
                 return ($rows = parent::query($sql)) ? $rows : false;
